4b82bc73a3d4a2a46c09c2e824ba077956640769
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2021-01-13 18:18:57 -0500

    random word selection

---
+++ hman.py
@@ -0,0 +1,7 @@
+import random 
+
+diff_requirements = ["python", "javascript", "html", "css", "react", "mongoDB", "api", "frontend", "backend", "fullstack"]
+
+words = random.choice(diff_requirements)
+
+print(words)
