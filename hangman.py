41bd01e06b0b6b45d5ec7556662e957032c9bd91
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-01-14 08:58:24 -0500

    a start

--- hangman.py
+++ hangman.py
@@ -1,5 +1,19 @@
-words = ['what', 'is', 'the', 'secret', 'word', 'kristin', 'wiig']
-secret_word = words[5]
+# import Mongo
+from pymongo import MongoClient
+client = MongoClient()
+db = client.test_database
+
+hman_challenge = requests.get('hangman-api')
+# words = ['what', 'is', 'the', 'secret', 'word', 'kristin', 'wiig']
+# hman_challenge.json.get(word)
+
+words = {
+    'choices': hman_challenge.json.get(word_list)
+    'userchoice': []
+}
+
+word_index = random.randint(0, len(words))
+secret_word = words[choices[word_index]]
 
 def hash(string):
     length = len(string)
@@ -8,15 +22,22 @@ def hash(string):
 
 hashed = hash(secret_word)
 
-def check_word(l):
-    secret_word_list = [char for char in secret_word]
+def check_word(l, word):
+    secret_word_list = [char for char in word]
     to_reveal = []
     for i in range(len(secret_word_list)):
         if l == secret_word_list[i]:
-            new = hashed[:i] + secret_word[i] + hashed[i:]
-            new_hashed = new[:-1]
-    if 
+            to_reveal.append(i)
+    for i in range(len(to_reveal)):
+        index = to_reveal[i]
+        new = hashed[:index] + secret_word[index] + hashed[index:]
+        new_hashed = new[:-1]
+    if len(to_reveal) == 0:
+        return 'Not found!'
+
     return new_hashed
 
 letter = input('Enter a letter: ')
-print(check_word(letter))
+# counter for turns: the length of the word
+def turns()
+check_word(letter, secret_word)
