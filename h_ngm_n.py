6bbd899965d730d972dedb05c90c15a69c2e24b0
richardleung1 <richardl57549@gmail.com>
2021-01-14 10:44:46 -0500

    solution

--- h_ngm_n.py
+++ h_ngm_n.py
@@ -1,5 +1,9 @@
 import random
 
+words = ['SHINRINYOKU', 'KOMOREBI', 'KUIDAORE', 'TSUNDOKU', 'WABI-SABI', 'KINTSUGI', 'MONO NO AWARE', 'IRUSU', 'KAROSHI', 'SHOGANAI', 'NATSUKASHII']
+
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
 def check_win(string1, string2):
     if string1 == string2:
         return True
@@ -9,22 +13,24 @@ def check_win(string1, string2):
 def win_message():
     print('=D')
     print('Congratulations! You have completed the word puzzle. You are now free.')
-    revive = input('Continue? (y/n)')
-    replay(revive)
+    replay()
 
 def loss_message(mystery_word):
     print('\n')
     print('The correct word was ', mystery_word)
     print('\n')
     print('=O')
-    print('You have used all your guesses and have been hanged! Run again to have another chance at life.')
-    revive = input('Continue? (y/n)')
-    replay(revive)
+    print('You have used all your guesses and have been hanged!')
+    replay()
     
-def replay(revive):
+def replay():
+    revive = input('Replay? (y/n) ')
     if revive == 'y':
-        failed_guess_count = 0
         hangman()
+    if revive == 'n':
+        print('Thank you for playing')
+    else:
+        replay()
 
 def print_build(count):
     
@@ -40,19 +46,19 @@ def print_build(count):
     for part in build[count]:
         print(part)
 
-
-def hangman():
-
+def welcome_message():
     print('\n')
     print('WELCOME TO JAPANESE HANGMAN!!!')
     print('WHERE YOU ARE A CRIMINAL ABOUT TO BE HANGED')
     print('SOLVE THE PUZZLE TO BE FREED')
 
-    words = ['SHINRINYOKU', 'KOMOREBI', 'KUIDAORE', 'TSUNDOKU', 'WABI-SABI', 'KINTSUGI', 'MONO NO AWARE', 'IRUSU', 'KAROSHI', 'SHOGANAI', 'NATSUKASHII']
+def hangman():
+
+    welcome_message()
+
     mystery_word = words[random.randint(0, 10)]
     dashes = ''
 
-    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     for i in range(len(mystery_word)):
         if mystery_word[i] in alphabet:
             dashes += '_'
@@ -72,15 +78,16 @@ def hangman():
 
         if check_win(dashes, mystery_word):
             win_message()
-            break
-            
         
         print(f'You have ', 7 - failed_guess_count, ' guesses remaining')
         print('Letters guessed: ', letters_guessed)
 
         guess = input('Guess a letter: ').upper()
 
-        if len(guess) > 1 or guess not in alphabet:
+        if guess == mystery_word:
+            win_message()
+
+        elif len(guess) > 1 or guess not in alphabet:
 
             guess = input('GUESS A LETTER: ').upper()
             print('Thank you! =D')
