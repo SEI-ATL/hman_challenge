06f9e1b58acad5504cd1fd75a1b5572e3bd81841
alexjbustillos <alexjbustillos@gmail.com>
2021-01-14 10:43:13 -0500

    finished

--- hangman.py
+++ hangman.py
@@ -1,63 +1,191 @@
-import random
+from random import random
+from math import floor
 
-words = ['one', 'fire', 'water', 'ice', 'fairy', 'steel', 'rock', 'ground']
+# in itialize game
+secret_word = ""
+letters_left = 0
+hangman_guesses_left = 6
+letters_guessed = []
+errors = 0
+underscored_secret_word = ""
+secret_words = ["development", "random", "postman", "mountain", "python", "wrong", "javascript", "software", "engineer"]
+# visual variables
+hangman1 = '''
+______
+|    |
+|    O
+|
+|
+|__________
+'''
+hangman2 = '''
+______
+|    |
+|    O
+|    |
+|
+|__________
+'''
+hangman3 = '''
+______
+|    |
+|    O
+|   /|
+|
+|__________
+'''
+hangman4 = '''
+______
+|    |
+|    O
+|   /|\\
+|
+|__________
+'''
+hangman5 = '''
+______
+|    |
+|    O
+|   /|\\
+|   /
+|__________
+'''
+hangman6 = '''
+______
+|    |
+|    O
+|   /|\\
+|   / \\
+|__________
+'''
+hangman_table = [
+  f'\n{hangman6}\n',
+  f'\n{hangman5}\n',
+  f'\n{hangman4}\n',
+  f'\n{hangman3}\n',
+  f'\n{hangman2}\n',
+  f'\n{hangman1}\n'
+]
 
-def random_word():
-    word = random.choice(words)
-    return word.lower()
+def game_init():
+    global secret_word
+    global letters_left
+    global hangman_guesses_left
+    global letters_guessed
+    global errors
+    global underscored_secret_word
 
+    random_index = floor(random() * 6)
+    secret_word = secret_words[random_index]
+    letters_left = len(secret_word)
+    hangman_guesses_left = 6
+    letters_guessed = []
 
-def game(word):
-    underscores = '_' * len(word)
-    correct_guess = False
-    guessed_letters = []
-    guessed_words = []
-    attempts = 7
-    print('Play the game')
-    print('You have ', attempts, 'guesses')
-    print(underscores)
-    while not correct_guess and attempts > 0:
-         guess = input('Guess a letter or the whole word here: ').lower()
-         if len(guess) == 1 and guess.isalpha():
-             if guess in guessed_letters:
-                 print('You already guessed the letter ', guess)
-             elif guess not in word:
-                 print(guess, ' is not the correct word')
-                 print('You have ', attempts, ' left')
-                 attempts -= 1
-                 guessed_letters.append(guess)
-             else:
-                 print(guess, ' is the correct word!')
-                 guessed_letters.append(guess)
-                 word_list = list(underscores)
-                 indexes = [i for i, letter in enumerate(
-                     word) if letter == guess]
-                 for index in indexes:
-                     word_list[index] = guess
-                 complete_word = ''.join(underscores)
-                 if '_' not in complete_word:
-                     correct_guess = True
-         elif len(guess) == len(word) and guess.isalpha():
-             if guess in guessed_words:
-                 print('You already guessed that word')
-             elif guess != word:
-                 print('Not the correct word')
-                 attempts -= 1
-                 guessed_words.append(guess)
-             else:
-                 correct_guess = True
-                 complete_word = word
-         else:
-             print('Please only guess letters or words')
-     if correct_guess == True:
-         print('You gueessed the correct word!')
-     else:
-         print('You ran out of guesses, the correct word was ', word)
+    i = 0
+    while i < len(secret_word):
+        underscored_secret_word = underscored_secret_word + "_ "
+        i += 1
 
+def win():
+    print('You won!')
+    print(f'The word was {secret_word}')
 
- def play_game():
-     word = random_word()
-     game(word)
+def lose():
+    print('HMAN: :(')
+    print(f"the secret word was {secret_word}")
 
+def add_to_guesses(guess):
+    letters_guessed.append(guess)
 
- play_game()
+def success(guess):
+  global underscored_secret_word
+  global win
+  print("Correct")
+  add_to_guesses(guess)
+  index = 0
+  for letter in secret_word:
+    if letter == guess:
+      # handles the case for when the first letter in the secret word is the guess
+      if index == 0:
+        underscored_secret_word = guess + underscored_secret_word[1:] # apple -> a _ _ _ _
+      # handles the case for when the guessed letter is not the first or the last letter
+      elif index < (len(secret_word) - 1):
+        underscored_secret_word = underscored_secret_word[0:(index * 2)] + guess + " " + underscored_secret_word[(index * 2 + 2):]
+      # handles the case for when the guessed letter is the last letter in the secret word
+      else:
+        underscored_secret_word = underscored_secret_word[:-2] + guess
+    index += 1
+  # check if all letters have been guessed, and if so run win() otherwise we'll continue the game
+  for letter in underscored_secret_word:
+    if letter == "_":
+      continue_game()
+      return
+  win()
+
+def print_hangman(incorrect_guesses_left):
+    print(hangman_table[incorrect_guesses_left])
+
+def failure(guess):
+    global hangman_guesses_left
+    add_to_guesses(guess)
+    hangman_guesses_left -= 1
+    print_hangman(hangman_guesses_left)
+    continue_game()
+
+def already_guessed():
+    print('You already guessed that')
+    continue_game()
+
+def continue_game():
+    global errors
+    print(f'You have {hangman_guesses_left} guesses left.')
+    print(f'You have guessed: {letters_guessed}')
+    print(f'The word you need: {underscored_secret_word}')
+    # can the user continue to make guesses or is the game over
+    if hangman_guesses_left > 0:
+        guess = input('Guess a letter:\n').lower()
+        if guess == secret_word:
+            win()
+            return
+        # handle special cases here
+        if len(guess) > 1 and errors < 1:
+            print('It looks like you accidentily entered more than one character or you guessed the wrong word.')
+            errors += 1
+            continue_game()
+            return
+        try:
+            int(guess)
+            print('It looks like you accidentily entered a number.')
+            errors += 1
+            continue_game()
+            return
+        except ValueError:
+            pass
+
+        if guess in letters_guessed:
+            already_guessed()
+            return
+
+        for letter in secret_word:
+            if letter == guess:
+                success(guess)
+                return
+        failure(guess)
+        return
+    else:
+        lose()
+# game loop that is going to continue running the game, call game_init() and initially run the continue_game(). Prompt user to see if they wanna continue playing
+
+looping = True
+game_init()
+continue_game()
+while looping:
+    should_continue = input('Would you like to continue playing? yes or no\n').lower()
+    if should_continue == 'yes' or should_continue == 'y':
+        game_init()
+        continue_game()
+    elif should_continue == 'no' or should_continue == 'n':
+        print('Thanks for playing')
+        looping = False
+    else:
+        print("I'm not sure whether you mean yes or no.\n")
