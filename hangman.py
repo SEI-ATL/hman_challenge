995e3e708d9e7c7763f6bd3e2d26e1bfa3756b12
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2021-01-13 22:18:54 -0800

    Hangman

---
+++ hangman.py
@@ -0,0 +1,29 @@
+import random
+
+def win_check(str1, str2):
+    if str1 == str2:
+        return True
+    else:
+        return False
+
+def hangman():
+    print('Solve the puzzle!')
+    print('Hint: Thinking of a Footballer')
+
+
+    answers = ['RONALDINHO', 'RONALDO', 'CRUYFF', 'MARADONA', 'MESSI', 'DEMBELE', 'GRIEZMANN', 'INIESTA', 'ZIDANE', 'ROONEY', 'HENRY']
+    random_answer = answers[random.randint(0, 10)]
+    print(random_answer)
+    space = ''
+
+
+    alphabet ='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    
+    for i in range(len(random_answer)):
+        if random_answer[i] in alphabet:
+            space += '_'
+        else:
+            space += 'random_answer[i]'
+
+
+hangman()
